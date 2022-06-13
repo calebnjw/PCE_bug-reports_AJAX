c42607f7b1fca2da978f72ac801945dd11b2cea0
calebnjw <lelebng@gmail.com>
2022-06-13 21:24:22 +0800

    feature selection using dropdown list

--- public/script.js
+++ public/script.js
@@ -17,34 +17,6 @@ const submitBugButton = () => {
   submitButton.addEventListener('click', submitBug);
 };
 
-const submitBug = async () => {
-  const errorContainer = document.getElementById('error-message');
-
-  const problemText = document.querySelector('input[name="problem"]').value;
-  const errorText = document.querySelector('input[name="error"]').value;
-  const featureSel = document.querySelector('input[name="feature"]:checked').value;
-
-  console.log(problemText, errorText, featureSel);
-
-  if (problemText === '' || errorText === '') {
-    errorContainer.innerText = 'Please fill in the form before submitting.';
-    errorContainer.style.display = 'block';
-  } else {
-    axios.post('/bugs/new', {
-      problem: problemText,
-      error: errorText,
-      feature: featureSel,
-    }).catch((error) => {
-      console.log(error);
-    });
-
-    errorContainer.style.display = 'none';
-    document.querySelector('input[name="problem"]').value = '';
-    document.querySelector('input[name="error"]').value = '';
-  }
-
-};
-
 const submitFeatureButton = () => {
   const submitButtonContainer = document.getElementById('new-feature');
   const submitButton = document.createElement('button');
@@ -54,38 +26,25 @@ const submitFeatureButton = () => {
   submitButton.addEventListener('click', submitFeature);
 };
 
-const submitFeature = async () => {
-  const featureName = document.querySelector('input[name="feature-name"]').value;
-
-  axios.post('/features/new', {
-    name: featureName,
-  }).catch((error) => {
-    console.log(error);
-  });
-
-  document.querySelector('input[name="feature-name"]').value = '';
-  window.location.href = '/';
-};
-
-const createFeatureSelection = async () => {
+const createFeatureSelect = async () => {
   const formArea = document.getElementById('form-area');
-  const featureForm = document.createElement('form');
-  formArea.append(featureForm);
 
   const featureList = await getFeatures();
+  const featureLabel = document.createElement('label');
+  const featureSelect = document.createElement('select');
+  formArea.append(featureLabel, featureSelect);
+
+  featureLabel.innerText = 'Feature:';
+  featureLabel.htmlFor = 'feature';
+  featureSelect.name = 'feature';
+  featureSelect.id = 'feature';
+
   featureList.forEach((feature) => {
-    const featureInput = document.createElement('input');
-    const featureLabel = document.createElement('label');
+    const featureOption = document.createElement('option');
 
-    featureInput.type = 'radio';
-    featureInput.id = feature.name;
-    featureInput.value = feature.name;
-    featureInput.name = 'feature';
-
-    featureLabel.htmlFor = feature.name;
-    featureLabel.innerText = feature.name;
-
-    featureForm.append(featureInput, featureLabel);
+    featureOption.value = feature.name;
+    featureOption.innerText = feature.name;
+    featureSelect.append(featureOption);
   });
 
   submitBugButton();
@@ -104,6 +63,46 @@ const createBugList = async () => {
   });
 };
 
-createFeatureSelection();
+const submitBug = async () => {
+  const errorContainer = document.getElementById('error-message');
+
+  const problemText = document.querySelector('input[name="problem"]').value;
+  const errorText = document.querySelector('input[name="error"]').value;
+  const featureSel = document.querySelector('select[name="feature"]').value;
+
+  console.log(problemText, errorText, featureSel);
+
+  if (problemText === '' || errorText === '') {
+    errorContainer.innerText = 'Please fill in the form before submitting.';
+    errorContainer.style.display = 'block';
+  } else {
+    // axios.post('/bugs/new', {
+    //   problem: problemText,
+    //   error: errorText,
+    //   feature: featureSel,
+    // }).catch((error) => {
+    //   console.log(error);
+    // });
+
+    errorContainer.style.display = 'none';
+    document.querySelector('input[name="problem"]').value = '';
+    document.querySelector('input[name="error"]').value = '';
+  }
+};
+
+const submitFeature = async () => {
+  const featureName = document.querySelector('input[name="feature-name"]').value;
+
+  axios.post('/features/new', {
+    name: featureName,
+  }).catch((error) => {
+    console.log(error);
+  });
+
+  document.querySelector('input[name="feature-name"]').value = '';
+  window.location.href = '/';
+};
+
+createFeatureSelect();
 createBugList();
 submitFeatureButton();
